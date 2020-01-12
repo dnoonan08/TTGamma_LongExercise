a506e1741cf28261da39f5d2e5f6f639b2898c66
wamorkart <wamorkart.t@husky.neu.edu>
2020-01-11 18:00:08 -0600

    strip parts of code and add instructions

--- Fitting/misIDScaleFactorFitting.py
+++ Fitting/misIDScaleFactorFitting.py
@@ -2,9 +2,10 @@ from ROOT import TFile, TFractionFitter, TObjArray
 
 import pprint
 
+## open root file, created from the SaveHistogramsToRoot step, containing the e+gamma mass distributions 
 _file = TFile("../RootFiles/MisID_Output_electron.root")
 
-
+## List of systematics
 systematics  = ["nominal",
                 "FSRDown",
                 "FSRUp",
@@ -32,30 +33,44 @@ systematics  = ["nominal",
 
 results = {}
 
-data = _file.Get("dataObs")
+## Get data from the input root file
+data = ?
     
+## Loop over the list of systematics    
 for syst in systematics:
-
-    misID = _file.Get(f"MisIDele_{syst}")
+    
+    ## Get histogram from the MisIDele category
+    misID = _file.Get(?)
+    
     otherMC = _file.Get(f"Other_{syst}")
-    otherMC.Add(_file.Get(f"WGamma_{syst}"))
-    otherMC.Add(_file.Get(f"ZGamma_{syst}"))
+    ## Add histograms from WGamma and ZGamma categories
+    otherMC.Add(?)
+    otherMC.Add(?)
 
 
     mc = TObjArray(2)
-    mc.Add(misID)
-    mc.Add(otherMC)
+    ## Add the histograms from the MC to the mc array
+    mc.Add(?)
+    mc.Add(?)
 
-    fit = TFractionFitter(data, mc,"q")
+    ## Fit the MC histograms to data 
+    fit = TFractionFitter(?)
     
-    status = int(fit.Fit())
-
-    fitResults = fit.GetFitter().Result().Parameters()
-
+    ## fit.Fit() actually performs the fit
+    ## check the fit status
+    status = ?
+    
+    ## status==0 corresponds to fits that converged
+    
+    ## Get the value of fit parameters
+    fitResults = ?
+    
+    ## In order to calculate the electron mis-identification scale factor (SF), we extract the value of the fit parameter for the misID MC and use it to calculate the fraction of mis-identified electrons
     misIDSF  = data.Integral()*fitResults[0]/mc[0].Integral()
     if not status==0:
         print (f"Error in fit while processing {syst} sample: exit status {status}")
-
+        
+    ## Fill the dictionary "results" with the misID SF for each systematic
     results[syst] = misIDSF
 
     del fit
--- Fitting/photonPurityFitting.py
+++ Fitting/photonPurityFitting.py
@@ -2,9 +2,10 @@ from ROOT import TFile, TFractionFitter, TObjArray
 
 import pprint
 
+## open root file, created from the SaveHistogramsToRoot step, containing charged hadron isolation distributions which have been grouped into isolated and nonprompt categories
 _file = TFile("../RootFiles/Isolation_Output.root")
 
-
+## List of systematics
 systematics  = ["nominal",
                 "FSRDown",
                 "FSRUp",
@@ -32,40 +33,57 @@ systematics  = ["nominal",
 
 results = {}
 
-data = _file.Get("dataObs")
-    
+## Get data from the input root file
+data = ?
+
+## Loop over the list of systematics
 for syst in systematics:
-
-    mc = TObjArray(2)
-    mc.Add(_file.Get(f"Isolated_{syst}"))
-    mc.Add(_file.Get(f"NonPrompt_{syst}"))
-
-    fit = TFractionFitter(data, mc,"q")
     
-    status = int(fit.Fit())
-
-    fitResults = fit.GetFitter().Result().Parameters()
-
+    ## Add histograms from Isolated and NonPrompt categories to the array 'mc'
+    mc = TObjArray(2)
+    mc.Add(_file.Get(?))
+   
+     ## Fit the MC histograms to data 
+    fit = TFractionFitter(?)
+    
+    ## fit.Fit() actually performs the fit
+    ## check the fit status
+    status = ?
+    
+    ## status==0 corresponds to fits that converged, and we can then obtain the fit result 
+    fitResults = ?
+    
+    ## Calculating the scale factor for isolated photons 
     isolatedSF  = data.Integral()*fitResults[0]/mc[0].Integral()
-    nonPromptSF = data.Integral()*fitResults[1]/mc[1].Integral()
-
+    
+    ## Similarly, calculate the scale factor for the nonprompt photons
+    nonPromptSF = ?
+    
+    ## Calculate the number of events with isolated photons, using the isolatedSF
     isolatedRate = mc[0].GetBinContent(1)*isolatedSF
-    nonPromptRate = mc[1].GetBinContent(1)*nonPromptSF
+    ## Calculate the number of events with nonPrompt photons, using the isolatedSF
+    nonPromptRate = ?
+   
     totalRate = (isolatedRate + nonPromptRate)
 
     if not status==0:
         print (f"Error in fit while processing {syst} sample: exit status {status}")
     
-    phoPurity = isolatedRate / totalRate
+    ## Now that we know the number of events with isolated photons and the total number of events, we can calculate the photon Purity
+    phoPurity = ?
 
-    fitError_iso = fit.GetFitter().Result().ParError(0)
-    fitError_np = fit.GetFitter().Result().ParError(1)
-    isoError = data.Integral()*fitError_iso*mc[0].GetBinContent(1)/mc[0].Integral()
-    npError = data.Integral()*fitError_np*mc[1].GetBinContent(1)/mc[1].Integral()
-
+    ## Get the error on the fit parameter for isolated and nonprompt category
+    fitError_iso = ?
+    fitError_np = ?
     
+    ## Calculate the error on isolatedRate and nonPromptRate
+    isoError = ?
+    npError = ?
+
+    ## Now we can also calculate the error on photon Purity
     phoPurityErr = ((isoError * (1 + phoPurity) / totalRate)**2 + (npError*phoPurity/totalRate)**2)**0.5
 
+    ## Fill the dictionary "results" with the photonPurity and error in photonPurity for each systematic
     results[syst] = (phoPurity, phoPurityErr)    
 
     del fit
