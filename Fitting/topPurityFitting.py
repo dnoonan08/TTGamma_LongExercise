e5befd879d947817a561c34c8add819cb7379395
wamorkart <wamorkart.t@husky.neu.edu>
2020-01-11 18:00:08 -0600

    strip parts of code and add instructions

--- Fitting/topPurityFitting.py
+++ Fitting/topPurityFitting.py
@@ -2,9 +2,10 @@ from ROOT import TFile, TFractionFitter, TObjArray
 
 import pprint
 
+## open root file, created from the SaveHistogramsToRoot step, containing M3 distributions 
 _file = TFile("../RootFiles/M3_Output.root")
 
-
+## List of systematics
 systematics  = ["nominal",
                 "FSRDown",
                 "FSRUp",
@@ -32,23 +33,34 @@ systematics  = ["nominal",
 
 results = {}
 
-data = _file.Get("dataObs")
-    
+## Get data from the input root file
+data = ?
+
+## Loop over the list of systematics
 for syst in systematics:
-
-    mc = TObjArray(2)
-    mc.Add(_file.Get(f"TopPair_{syst}"))
-    mc.Add(_file.Get(f"NonTop_{syst}"))
-
-    fit = TFractionFitter(data, mc,"q")
     
-    status = int(fit.Fit())
-
+    ## Add TopPair and NonTop histograms to the array 'mc'
+    ## Create an array 'mc' of histograms from TopPair and NonTop categories
+    mc = TObjArray(2)   
+    mc.Add(_file.Get(?))
+    
+    ## Fit the MC histograms to data 
+    fit = TFractionFitter(?)
+    
+    ## fit.Fit() actually performs the fit
+    ## check the fit status
+    status = ?
+    
+    ## status==0 corresponds to fits that converged
+    ## Now we can extract value of topPurity (fraction of events coming from top pair production) and the error on that value, topPurityErr for each systematic
     if not status==0:
         print (f"Error in fit while processing {syst} sample: exit status {status}")
-    topPurity = fit.GetFitter().Result().Parameters()[0]
-    topPurityErr = fit.GetFitter().Result().ParError(0)
-
+        
+    ## Get the value of fit parameter and the error for the TopPair MC category: 
+    topPurity = ?
+    topPurityErr = ?
+    
+    ## Fill the dictionary "results" with the topPurity and topPurityErr for each systematic
     results[syst] = (topPurity, topPurityErr)
 
     del fit
